295f31fa2adae8314393419553fbc105e4029611
V20000000000000 <kevinkang5880@gmail.com>
2024-03-16 22:22:03 +0800

    fix int to unsign int

--- alu_testing_program/alu_testing_program.cpp
+++ alu_testing_program/alu_testing_program.cpp
@@ -8,31 +8,32 @@
 #include <algorithm>
 #include <cmath>
 #include <iomanip>
+#include <bitset>
 
 using namespace std;
 
 // Function prototypes
-void parseInstruction(const string& line, int& reg1, int& reg2, int& shiftAmt, string& funct, string& carry);
+void parseInstruction(const string& line, unsigned int& reg1, unsigned int& reg2, unsigned int& shiftAmt, string& funct, bool& carry);
 string decodeFunction(const string& funct);
 bool testALU(const string& instruction, const string& expectedOutput, int lineNum);
-string zeroDetect(int num);
-string hasCarry(string binary1, string binary2);
+string zeroDetect(unsigned int num);
+bool hasCarry(string binary1, string binary2);
 void removeSpaces(string &str);
-string intToBinaryString(int num, unsigned int length);
-int binaryToDecimal(string binaryString);
-string decimalToHexadecimal(int decimalNum);
+string intToBinaryString(unsigned int num, unsigned int length);
+unsigned int binaryToDecimal(string binaryString);
+string decimalToHexadecimal(unsigned int decimalNum);
 
 void removeSpaces(string &str) {
     str.erase(remove_if(str.begin(), str.end(), ::isspace), str.end());
 }
 
-string decimalToHexadecimal(int decimalNum) {
+string decimalToHexadecimal(unsigned int decimalNum) {
     stringstream ss;
     ss << setw(8) << setfill('0') << hex << decimalNum; // Convert decimal to hexadecimal
     return ss.str();
 }
 
-string intToBinaryString(int num, unsigned int length) {
+string intToBinaryString(unsigned int num, unsigned int length) {
     string binary = "";
     while (num > 0) {
         binary = (num % 2 == 0 ? "0" : "1") + binary;
@@ -45,9 +46,9 @@ string intToBinaryString(int num, unsigned int length) {
     return binary;
 }
 
-int binaryToDecimal(string binaryString) {
-    int decimal = 0;
-    int exponent = 0;
+unsigned int binaryToDecimal(string binaryString) {
+    unsigned int decimal = 0;
+    unsigned int exponent = 0;
 
     // Iterate through the binary string from right to left
     for (int i = binaryString.length() - 1; i >= 0; --i) {
@@ -61,8 +62,8 @@ int binaryToDecimal(string binaryString) {
     return decimal;
 }
 
-string hasCarry(string binary1, string binary2) {
-    string carry = "0"; // Initialize carry to false
+bool hasCarry(string binary1, string binary2) {
+    bool carry = false; // Initialize carry to false
 
     // Ensure both binary numbers have the same number of bits
     int maxLength = max(binary1.length(), binary2.length());
@@ -71,18 +72,18 @@ string hasCarry(string binary1, string binary2) {
 
     // Iterate through each bit and check for carry
     for (int i = maxLength - 1; i >= 0; --i) {
-        int sum = (binary1[i] - '0') + (binary2[i] - '0') + stoi(carry);
+        int sum = (binary1[i] - '0') + (binary2[i] - '0') + carry;
         if (sum > 1) {
-            carry = "1"; // If sum is greater than 1, set carry to true
+            carry = true; // If sum is greater than 1, set carry to true
         } else {
-            carry = "0"; // Otherwise, set carry to false
+            carry = false; // Otherwise, set carry to false
         }
     }
 
     return carry; // Return the final carry value
 }
 
-string zeroDetect(int num) {
+string zeroDetect(unsigned int num) {
     if (num == 0) {
         return "1"; // Return "1" if num is zero
     } else {
@@ -90,7 +91,7 @@ string zeroDetect(int num) {
     }
 }
 
-void parseInstruction(const string& line, int& reg1, int& reg2, int& shiftAmt, string& funct, string& carry) {
+void parseInstruction(const string& line, unsigned int& reg1, unsigned int& reg2, unsigned int& shiftAmt, string& funct, bool& carry) {
     stringstream ss(line);
     string temp1, temp2, temp3;
     string hex1, hex2, hex3;
@@ -122,9 +123,11 @@ string decodeFunction(const string& funct) {
 
 bool testALU(const string& instruction, const string& actualOutput, int lineNum) {
     // Parse instruction
-    int reg1, reg2, shiftAmt;
+    unsigned int reg1, reg2;
+    unsigned int shiftAmt;
     string time;
-    string funct, carry;
+    string funct;
+    bool carry;
     parseInstruction(instruction, reg1, reg2, shiftAmt, funct, carry);
 
     cout << "Funct: " << funct << endl;
@@ -133,16 +136,16 @@ bool testALU(const string& instruction, const string& actualOutput, int lineNum)
     string decodedFunct = decodeFunction(funct);
 
     // Perform ALU operation based on function
-    int result;
+    unsigned int result;
     if (decodedFunct == "Add unsigned") {
         result = reg1 + reg2;
         cout << ">> Addu Result, Src1, Src2" << endl;
     } else if (decodedFunct == "Subtract unsigned") {
         result = reg1 - reg2;
         if(reg1 > reg2) {
-            carry = "0";
+            carry = false;
         } else {
-            carry = "1";
+            carry = true;
         }
         cout << ">> Subu Result, Src1, Src2" << endl;
     } else if (decodedFunct == "And") {
@@ -152,16 +155,10 @@ bool testALU(const string& instruction, const string& actualOutput, int lineNum)
         result = reg1 >> shiftAmt;
         cout << ">> Srl Result, Src1, Shamt" << endl;
     }
+    time = to_string(20 * lineNum);
     
     // Convert result to binary string
-    string expectedOutput;
-    for (int i = 31; i >= 0; --i) {
-        expectedOutput += to_string((result >> i) & 1);
-    }
-
-    time = to_string(20 * lineNum);
-
-    expectedOutput = time + "," + expectedOutput + "," + zeroDetect(result) + "," + carry;
+    string expectedOutput = time + "," + intToBinaryString(result, 32) + "," + zeroDetect(result) + "," + (carry ? "1" : "0");
 
     // Compare actual output with expected output
     if (expectedOutput != actualOutput) {
@@ -170,10 +167,10 @@ bool testALU(const string& instruction, const string& actualOutput, int lineNum)
         cout << "Expected: " << expectedOutput << endl;
         cout << "Actual: " << actualOutput << endl;
         return false;
-    }else{
+    } else {
         cout << "ALU_result: " << intToBinaryString(result, 32) << " (0x" << decimalToHexadecimal(result) << ") (" << result << ")" << endl;
         cout << "Zero: " << zeroDetect(result) << endl;
-        cout << "Carry: " << carry << endl;
+        cout << "Carry: " << (carry ? "1" : "0") << endl;
     }
     return true;
 }
@@ -194,7 +191,7 @@ int main() {
     for (int i = 0; i < numTests; ++i) {
         unsigned int reg1 = rand() % 4294967295;
         unsigned int reg2 = rand() % 4294967295;
-        int shiftAmt = rand() % 32;
+        unsigned int shiftAmt = rand() % 32;
         string funct;
         switch (rand() % 4) {
             case 0: funct = "001001"; break;
@@ -253,3 +250,4 @@ int main() {
 
     return 0;
 }
+
