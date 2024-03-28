d561dd3a690c398fdb9b9879434c5a5e84632c1a
V20000000000000 <kevinkang5880@gmail.com>
2024-03-28 22:22:11 +0800

    Multiplicand tb_Multiplicand

--- PA1/unsigned_complete_multiplier/Multiplicand.v
+++ PA1/unsigned_complete_multiplier/Multiplicand.v
@@ -5,4 +5,21 @@ module Multiplicand (
     input rst
 );
 
+    reg [31:0] multiplicand;
+
+    always @(posedge rst)
+    begin
+        multiplicand <= 32'b0;
+    end
+
+    always @(posedge w_ctrl_Multiplicand)
+    begin
+        if(!rst)
+        begin
+            multiplicand <= multiplicand_in;
+        end
+    end
+
+    assign multiplicand_out = multiplicand;
+
 endmodule
