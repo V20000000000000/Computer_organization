a620a3ffb227d1f835d853a15a87ada9ec6da80c
V20000000000000 <kevinkang5880@gmail.com>
2024-05-27 21:55:24 +0800

    complete ver

--- PA3/Part3/Forwarding.v
+++ PA3/Part3/Forwarding.v
@@ -1,48 +1,43 @@
-module Forwarding
-(
+module Forwarding(
+    // output
+    output reg [1:0] Forwarding_A,
+    output reg [1:0] Forwarding_B,
+    // input
     input [4:0] Rs_addr,
     input [4:0] Rt_addr,
     input [4:0] EX_Mem_Rd_addr,
     input [4:0] Mem_WB_Rd_addr,
     input EX_Mem_RegWrite,
-    input Mem_WB_RegWrite,
-    output reg [1:0] Forwarding_A,
-    output reg [1:0] Forwarding_B
+    input Mem_WB_RegWrite
 );
 
-    //Forwarding_A (Rs == Rd && reg_w == 1 && rd != 0): 00 -> No Forwarding, 01 -> Forwarding from Mem_WB, 10 -> Forwarding from EX_Mem
-
-    always @(*)
-    begin
-        if ((Rs_addr == EX_Mem_Rd_addr) && EX_Mem_RegWrite && EX_Mem_Rd_addr != 0)
-        begin
-            Forwarding_A = 10;
+    // Forwarding logic combined into a single always block
+    always @(*) begin
+        // Initialize forwarding control signals to no forwarding
+        Forwarding_A = 2'b00;
+        Forwarding_B = 2'b00;
+        
+        // Check EX hazard first
+        if (EX_Mem_RegWrite && (EX_Mem_Rd_addr != 0)) begin
+            // Forwarding A
+            if (EX_Mem_Rd_addr == Rs_addr) begin
+                Forwarding_A = 2'b10;
+            end
+            // Forwarding B
+            if (EX_Mem_Rd_addr == Rt_addr) begin
+                Forwarding_B = 2'b10;
+            end
         end
-        else if ((Rs_addr == Mem_WB_Rd_addr) && Mem_WB_RegWrite && Mem_WB_Rd_addr != 0) //&& (EX_Mem_Rd_addr != Rs_addr)
-        begin
-            Forwarding_A = 01;
-        end
-        else
-        begin
-            Forwarding_A = 00;
-        end
-    end
-
-    //Forwarding_B (Rt == Rd && reg_w == 1 && rd != 0): 00 -> No Forwarding, 01 -> Forwarding from Mem_WB, 10 -> Forwarding from EX_Mem
-
-    always @(*)
-    begin
-        if ((Rt_addr == EX_Mem_Rd_addr) && EX_Mem_RegWrite && EX_Mem_Rd_addr != 0)
-        begin
-            Forwarding_B = 10;
-        end
-        else if ((Rt_addr == Mem_WB_Rd_addr) && Mem_WB_RegWrite && Mem_WB_Rd_addr != 0) // && (EX_Mem_Rd_addr != Rt_addr)
-        begin
-            Forwarding_B = 01;
-        end
-        else
-        begin
-            Forwarding_B = 00;
+        // Check MEM hazard
+        else if (Mem_WB_RegWrite && (Mem_WB_Rd_addr != 0)) begin
+            // Forwarding A
+            if ((Forwarding_A == 2'b00) && (Mem_WB_Rd_addr == Rs_addr)) begin
+                Forwarding_A = 2'b01;
+            end
+            // Forwarding B
+            if ((Forwarding_B == 2'b00) && (Mem_WB_Rd_addr == Rt_addr)) begin
+                Forwarding_B = 2'b01;
+            end
         end
     end
 
